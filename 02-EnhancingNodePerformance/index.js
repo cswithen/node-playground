a135a46cd020718a29ac786983b98bf792e96276
cswithen <cody.swithenbank@gmail.com>
2022-05-30 21:23:07 -0700

    feat: blocking process
    included a function to simulate a blocking process on our sever

--- 02-EnhancingNodePerformance/index.js
+++ 02-EnhancingNodePerformance/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
-
 const app = express();
 
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
 app.get("/", (req, res) => {
+  doWork(5000);
   res.send("Hi there");
 });
 
-app.listen(3000);
+app.listen(3000, () =>
+  console.log(`
+Listening on port 3000
+
+http://localhost:3000/
+`)
+);
