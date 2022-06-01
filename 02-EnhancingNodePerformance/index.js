279f376f94195122cb6e48fa9a419026c114fd83
cswithen <cody.swithenbank@gmail.com>
2022-05-31 21:37:08 -0700

    feat(clusters): initialized cluster

--- 02-EnhancingNodePerformance/index.js
+++ 02-EnhancingNodePerformance/index.js
@@ -1,20 +1,36 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
-}
+// Is the file being exectuted in master mode?
+if (cluster.isMaster) {
+  // cause index.js to be executed *again* but in child
+  cluster.fork();
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  // Im a child, Im going to act like a server and do nothing else
+  const express = require("express");
+  const app = express();
 
-app.get("/", (req, res) => {
-  doWork(5000);
-  res.send("Hi there");
-});
+  const doWork = (duration) => {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  };
 
-app.listen(3000, () =>
-  console.log(`
+  app.get("/", (req, res) => {
+    doWork(5000);
+    res.send("Hi there");
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast!");
+  });
+
+  app.listen(3000, () =>
+    console.log(`
 Listening on port 3000
 
 http://localhost:3000/
 `)
-);
+  );
+}
