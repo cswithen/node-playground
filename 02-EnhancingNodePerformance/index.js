26d478639351057b61906ed17684161f152635e8
Cody Swithenbank <cody.swithenbank@gmail.com>
2022-06-06 08:15:45 -0700

    feat(js): refactor benchmark
    we are using the 'ab -c 50 -n 500 localhost:3000' function to get better fidelity on request data, specifically for a large number of requests

--- 02-EnhancingNodePerformance/index.js
+++ 02-EnhancingNodePerformance/index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
 
 // Is the file being exectuted in master mode?
@@ -10,27 +11,24 @@ if (cluster.isMaster) {
 } else {
   // Im a child, Im going to act like a server and do nothing else
   const express = require("express");
+  const crypto = require("crypto");
   const app = express();
 
-  const doWork = (duration) => {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  };
-
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there");
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send("Hi there");
+    })
   });
 
   app.get("/fast", (req, res) => {
     res.send("This was fast!");
   });
 
-  app.listen(3000, () =>
+  app.listen(3001, () =>
     console.log(`
-Listening on port 3000
+Listening on port 3001
 
-http://localhost:3000/
+http://localhost:3001/
 `)
   );
 }
